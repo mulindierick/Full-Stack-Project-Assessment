6da83fc605a2be4a238bf69d901a840b15cdd103
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2021-08-06 21:59:11 +0300

    finished level 100

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
+import data from "./exampleresponse.json";
 
 function App() {
+  let newData = data;
+  let x = "https://www.youtube.com/watch?v=HerCR8bw_GE";
+  let b = x.split("=");
+  console.log(b[b.length - 1]);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Video Recommendation</h1>
+        <h1>commendation</h1>
       </header>
+      <div>
+        {newData.map((item, index) => (
+          <iframe
+            title="x"
+            width="420"
+            height="315"
+            src={`https://www.youtube.com/embed/${
+              item.url.split("=")[item.url.split("=").length - 1]
+            }`}
+          ></iframe>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ client/src/sample.js
@@ -0,0 +1,3 @@
+let x = "https://www.youtube.com/watch?v=HerCR8bw_GE"
+let b = x.split("=")
+console.log(b[b.length -1])
